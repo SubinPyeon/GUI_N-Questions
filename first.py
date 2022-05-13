9c6e07e08d4f08bc387e46dc536b9e05ae2f5c7a
elmellamo <90952132+elmellamo@users.noreply.github.com>
2022-05-14 02:32:54 +0900

    Update first.py

--- first.py
+++ first.py
@@ -7,6 +7,8 @@ root = Tk()
 root.geometry("600x500")
 root.title("열고개 수수께끼")
 root.option_add("*Font","맑은고딕25")
+root.grid_rowconfigure(0, minsize=500)
+root.grid_columnconfigure(0,minsize=600)
 root.resizable(False, False)
 
 #프레임 전환하기
@@ -66,7 +68,7 @@ def isExist(id_one, pd_one):
     k=0
     
     while l:
-        a, b=l.split(' ',2)
+        a, b=l.split(' ')
         l=f.readline()
         
         if (a==id_one):
@@ -89,107 +91,107 @@ def isExist(id_one, pd_one):
     
 #화면 프레임 만들기
 frame1 = Frame(root) #맨 처음 화면
-frame1.grid(row=0, column=0, sticky="nsew") 
+frame1.grid(row=0, column=0, sticky=W+E+N+S) 
 
 frame2 = Frame(root) #회원가입 화면
-frame2.grid(row=0, column=0, sticky="nsew")
+frame2.grid(row=0, column=0, sticky=W+E+N+S)
 
 frame3 = Frame(root) #퀴즈 화면
-frame3.grid(row=0, column=0, sticky="nsew")
+frame3.grid(row=0, column=0,rowspan=4, columnspan=4, sticky=W+E+N+S)
 
 
 #frame1 >> 첫 화면 만들기
 #레이블 만들기
 tl = Label(frame1)
-tl.config(text="으쌰 열고개")
-tl.place(x=230, y=20)
-tl.pack()
+tl.config(text="⛰으쌰 열고개⛰", font=('맑은 고딕', 20,'bold'))
+tl['fg']='white'
+tl['bg']='forestgreen'
+tl.place(x=200, y=50)
 
 #id 칸
 idl = Label(frame1)
 idl.config(text="아이디")
-idl.place(x=100, y=100)
-idl.pack()
+idl.place(x=100, y=150)
 
 idt = Entry(frame1)
-idt.place(x=230, y=100)
-idt.pack()
+idt.place(x=230, y=150)
 
 
 #비밀번호 칸
 pd = Label(frame1)
 pd.config(text="비밀번호")
-pd.place(x=100, y = 150)
-pd.pack()
+pd.place(x=100, y = 200)
 
 pdt = Entry(frame1)
-pdt.place(x=230, y=150)
-pdt.pack()
+pdt.place(x=230, y=200)
 
 
 #시작칸 만들기 > 이후에 이벤트 처리해서 클릭, 엔터 두개 처리해야 함
 stl = Button(frame1)
 stl.config(text = "시작해볼까요?", command=lambda:[isExist(idt.get(), pdt.get())])
 stl.config(width = 20)
-stl.place(x=170, y=200)
-stl.pack()
+stl.place(x=170, y=300)
+
 
 #회원가입 만들기
 gol = Button(frame1)
 gol.config(text = "회원가입")
 gol.config(command = lambda:[openFrame(frame2)])
-gol.place(x=100, y=300)
-gol.pack()
-
-
+gol.place(x=230, y=350)
 
 
 #frame2>> 2번째 회원가입 화면만들기
 tl2 = Label(frame2)
-tl2.config(text = "사용자 등록")
-tl2.place(x=200, y=20)
-tl2.pack()
+tl2.config(text = "[사용자 등록]", font=('맑은 고딕', 18, 'bold'))
+tl2['bg']='darkgrey'
+tl2.place(x=70, y=20)
 
 idl2 = Label(frame2)
-idl2.config(text="아이디")
-idl2.place(x=100, y=150)
-idl2.pack()
-idlt = Entry(frame2)
-idlt.place(x=230, y=150)
-idlt.pack()
+idl2.config(text="아이디",font=('맑은 고딕', 13))
+idl2.place(x=70, y=100)
+
+idlt = Entry(frame2, width=15)
+idlt.place(x=180, y=100)
+
 
 #아이디 중복확인 버튼
 twiceid = Button(frame2)
-twiceid.config(text = "ID 중복확인", command=lambda:[twiceCheck(idlt.get())])
-twiceid.place(x=300, y=150)
-twiceid.pack()
+twiceid.config(text = "ID 중복확인", font=('맑은 고딕', 10), command=lambda:[twiceCheck(idlt.get())])
+twiceid.place(x=400, y=100)
+
 
 pd2 = Label(frame2)
-pd2.config(text="비밀번호")
-pd2.place(x=100, y=200)
-pd2.pack()
-pdt2 = Entry(frame2)
-pdt2.place(x=230, y=200)
-pdt2.pack()
+pd2.config(text="비밀번호",font=('맑은 고딕', 13))
+pd2.place(x=70, y=150)
+
+pdt2 = Entry(frame2, width=15)
+pdt2.place(x=180, y=150)
+
 
 pdtw = Label(frame2)
-pdtw.config(text="비번확인")
-pdtw.place(x=100, y=250)
-pdtw.pack()
-pdte = Entry(frame2)
-pdte.place(x=230, y=250)
-pdte.pack()
+pdtw.config(text="비번확인",font=('맑은 고딕', 13))
+pdtw.place(x=70, y=200)
+
+pdte = Entry(frame2, width=15)
+pdte.place(x=180, y=200)
+
 
 #비밀번호 일치확인 버튼 
 twicepd = Button(frame2)
-twicepd.config(text = "비밀번호 일치 확인", command=lambda:[pdSame(pdt2.get(), pdte.get())])
-twicepd.place(x=300, y=150)
-twicepd.pack()
+twicepd.config(text = "비밀번호 일치 확인", font=('맑은 고딕', 10),command=lambda:[pdSame(pdt2.get(), pdte.get())])
+twicepd.place(x=400, y=200)
+
+
+#이전 홈화면으로 돌아가기
+gohome = Button(frame2)
+gohome.config(text="🏠 이전 화면으로 돌아가기 🏠",font=('맑은 고딕', 13), command=lambda:[openFrame(frame1)])
+gohome.place(x=150, y=350)
+
 
 #아이디 중복체크, 비밀번호 일치까지 했을 때 생성하기 > 메시지 박스 함수 만들기
 prod = Button(frame2)
-prod.config(text = "생성하기")
-prod.pack()
+prod.config(text = "생성하기", font=('맑은 고딕', 13))
+prod.place(x=240,y=280)
 if(idcheck==1)and(pdcheck==1):
     prod.config(command=lambda:[a_member(idlt.get(), pdt2.get())])
     openFrame(frame3)
